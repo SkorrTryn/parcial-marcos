ddb38f71f1001ee77beeb17dc03de0c674f51c61
SkorrTryn <soydannyleo12@gmail.com>
2024-05-04 11:12:48 -0500

    bug

--- models/server.js
+++ models/server.js
@@ -24,6 +24,17 @@ class Server {
     this.app.use(express.json());
 
     this.app.use(express.static("public"));
+
+    this.app.use((req, res, next) => {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header(
+        "Access-Control-Allow-Headers",
+        "Authorization, X-API-KEY, Origin, X-Requested-With, Content-type, Accept, Access-Control-Allow-Request-Method"
+      );
+      res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+      res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+      next();
+    });
   }
 
   routes() {
